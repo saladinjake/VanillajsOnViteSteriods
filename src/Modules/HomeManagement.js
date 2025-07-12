f6973a57b30cd41a1ebe990901600fdfbe55133c
Victor Juwa <juwavictor@gmail.com>
2025-07-12 21:20:07 +0100

    chore: complete clean up

--- src/Modules/HomeManagement.js
+++ src/Modules/HomeManagement.js
@@ -8,8 +8,13 @@ export function init(params) {
 
   //  action in html string will work like this
   return {
-    hello() {
-      console.log('hello');
+    // define methods used in our template string or html pages both ways works
+    // if you follow the bind by data attributes. Home/index.html
+    hello({ event, element, dataset }) {
+      event.preventDefault();
+
+      const message = dataset.message;
+      // do something with event eg event.target.value if its an input binding
     },
   };
 }
